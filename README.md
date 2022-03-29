41d23b95b03e78dba67c166c6b64ad595c953e77
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-03-29 19:44:19 -0400

    refactor: remove strapi app (#2)

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Strapi application
+# Strapi plugin katsu-commerce
 
-A quick description of your strapi application
+A quick description of katsu-commerce.
--- src/api/prod/routes/prod.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * prod router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::prod.prod');
